86b3568f0c9640a3e02b758d0f49843bd621057e
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-07-03 08:42:57 +0100

    player move

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@ else if (randomNumber == 3){
 
 printMessage('Ruch komputera to: ' + computerMove);
 
+// player move
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+// let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+	playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+	playerMove ='papier';
+}
+else if(playerInput == '3'){
+	playerMove = 'nozyce'
+}
+// else(playerInput>=4){
+// 	printMessage('podales zla wartosc');
+// }
+
+printMessage('Twój ruch to: ' + playerMove);
