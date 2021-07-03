b1a55a798400648aced908e0edb97b0a5a9a0f8d
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-07-03 08:29:08 +0100

    add computer move

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+// Computer move
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+
+if(randomNumber == 1){
+	computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+	computerMove = 'papier';
+}
+else if (randomNumber == 3){
+	computerMove = 'nozyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-let playerMove = 'papier'
+printMessage('Ruch komputera to: ' + computerMove);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
